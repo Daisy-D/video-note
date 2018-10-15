9ae92b588a52f7730100ce6370a62d8acfff4751
代晓璐 <2428903784@qq.com>
2018-10-15 14:35:06 +0800

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="hello">
+  {{msg}}
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to My Home!'
+    }
+  }
+}
+</script>
+
+<style scoped>
+* {
+  color: #42b983;
+}
+</style>
